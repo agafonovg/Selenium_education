7c105e0b82cc5a168654f804322a8870ed3fc650
GeorgiyAga <agafon13@gmail.com>
2019-09-21 18:54:53 +0300

    выполнил еще одно задание, помучил гит

--- stepick2.2.py
+++ stepick2.2.py
@@ -9,4 +9,16 @@ url = "http://SunInJuly.github.io/execute_script.html"
 def calc(x):
 	return str(math.log(abs(12*math.sin(int(x)))))
 try:
-
+	browser = webdriver.Chrome(executable_path = path)
+	browser.get(url)
+	input1 = browser.find_element_by_id("input_value").text
+	input2=(calc(input1))
+	browser.find_element_by_id("answer").send_keys(input2)
+	browser.find_element_by_id("robotCheckbox").click()
+	browser.execute_script("window.scrollBy(0, 120);")
+	browser.find_element_by_id("robotsRule").click()
+	browser.find_element(By.XPATH, "//button[@type='submit']").click()
+    
+finally:
+	time.sleep(10)
+	browser.quit()
